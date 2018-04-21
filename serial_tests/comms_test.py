ccdff0b89f6a40289c50a6c988e86750483ccb73
Luis Gonzalez-Yante <ctangel14@gmail.com>
2018-04-21 22:47:19 +0000

    h

--- serial_tests/comms_test.py
+++ serial_tests/comms_test.py
@@ -1,5 +1,5 @@
 from Comms import Comms
-import threading, time, serial.tools.list_ports
+import threading, time, serial.tools.list_ports, json
 
 XBEE = {
     "vid": ["0403"],
@@ -20,50 +20,52 @@ def find_device(device):
               return True
     return False
 
-BAUDRATE = 9600
+def bytecode_to_hex(bytecode):
+    s = ""
+    #for byte in bytearray(str.encode(bytecode)):
+    for byte in bytearray(bytecode):
+        s = "%s-%s" % (s, "%02X" % (byte))
+    return s
 
-#XB01 = '/dev/cu.usbserial-DN041WOY' #XB01
-#XB22 = '/dev/cu.usbserial-DN02Z3LS' #XB22
 SERIAL_XB01_LOW = '\x41\x67\x21\xA9'
 SERIAL_XB22_LOW = '\x41\x75\xBC\x88'
 SERIAL_NUM_HIGH = '\x00\x13\xA2\x00' #same for all devices
-print "dab"
 try:
     if find_device(XBEE):
-        print XBEE.get('port')
-        #xb   = Comms(XBEE.get('port'), data_only=True)
-        xb   = Comms("/dev/ttyUSB0", data_only=True)
-    xb22 = Comms("/dev/ttyUSB1", data_only=True)
+        xb   = Comms(XBEE.get('port'), data_only=True)
 except:
-    print "failed..."
-    exit()
+    exit("Xbee connection Failed")
 
-print "Data only? " + str(xb.isDataOnly())
-print "switching..."
-xb.switchDataOnly()
-print "Data only? " + str(xb.isDataOnly())
-print "Mailbox empty? " + str(xb.isMailboxEmpty())
-print "message count: " + str(xb.messageCount())
+print "/** Prechecks"
+self = xb.getLocalAddr()
+print "\tMailbox empty? " + str(xb.isMailboxEmpty())
+print "\tmessage count: " + str(xb.messageCount())
 
+print "\n/** Sending Messages"
 data1 = "hey there! 1"
 data2 = "hey there! 2"
-data3 = "broadcasting"
+data3 = bytecode_to_hex(self[0])+bytecode_to_hex(self[1])
 dest = SERIAL_NUM_HIGH + SERIAL_XB01_LOW #XB22 -> XB01
-xb22.sendData(dest=dest, data = data1)
-xb22.sendData(dest=dest, data = data2)
-print "XB22 sent two messages..."
-print "Mailbox empty? " + str(xb.isMailboxEmpty())
-print "Message count: " + str(xb.messageCount())
-print xb.readMessage()
-print xb.readMessage()
-xb22.broadcastData(data3)
-print str(xb.messageCount())
-print xb.readMessage()
-print str(xb.readMessage())
+#xb.sendData(dest=dest, data = data1)
+#xb.sendData(dest=dest, data = data2)
+xb.broadcastData('{"code":"PING", "addr": "%s"}' % data3)
+print "\tXB22 sent two messages..."
 
-xb.getLocalAddr()
-
-xb.close()
-xb22.close()
-del xb
-exit()
+print "\n/** Reading Messages"
+target = None
+try:
+    while True:
+        if not xb.isMailboxEmpty():
+            data = json.loads(xb.readMessage().get('rx'))
+            print data
+            if data == "PING":
+                target = data.get('addr')
+                print target
+                data4 = bytecode_to_hex(self[0])+bytecode_to_hex(self[1])
+                xb.sendData(dest=target.replace("-","\\x"), data='{"code":"REPLY","addr":"%s"}' % data4)
+            elif data.get('code') == "REPLY":
+                reply = xb.readMessage()
+                print reply
+except (KeyboardInterrupt):
+    xb.close()
+    exit()
